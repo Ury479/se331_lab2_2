431413fd4535d6c6358a9e7d86460a567807b7e1
unknown <you@example.com>
2025-07-10 16:03:32 +0700

    新建github 仓库并引入db.json

---
+++ 331-intro-to-vite/src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="layout">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/events">Events</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+#layout {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-active {
+  color: #42b983;
+}
+
+h2 {
+  font-size: 20px;
+}
+</style>
